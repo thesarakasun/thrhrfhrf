b63efb3d13c29d10299e6e6ecbea17e63d0e6ae1
thesarakasun <88161607+thesarakasun@users.noreply.github.com>
2021-07-30 06:08:25 +0530

    Add files via upload

---
+++ checklogin.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Signup</title>
+    <link href="design.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <?php
+    include_once('header.php');
+    ?>
+
+    <div class="container-md w-50 mt-4  ">
+        <form onsubmit="return verifyPasswords()">
+
+            <div class="input-group mb-3">
+                <span class="input-group-text">First name </span>
+                <input type="text" class="form-control" placeholder="Eg : Kasun" aria-label="Username">
+                <span class="input-group-text">Last name</span>
+                <input type="text" class="form-control" placeholder="Eg : Perera" aria-label="Server">
+            </div>
+
+            <div class="mb-3">
+
+                <label for="exampleInputEmail1" class="form-label">Email address </label>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z" />
+                </svg>
+                <input type="email" class="form-control" placeholder="Eg : someone@mail.com" name="email" id="email" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
+                </svg>
+                <input type="password" class="form-control" name="password" id="pass1" onkeyup="hideError()" required>
+                <div id="passwordHelpBlock" class="form-text">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                </div>
+            </div>
+
+
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Re-type Password</label>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
+                </svg>
+                <input type="password" class="form-control" name="password" id="pass2" onkeyup="hideError()" required>
+
+            </div>
+            <div class="alert text-danger" role="alert" id="alert" style="visibility:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                    <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+                </svg>
+                Password field doesn't match with the Re-type Password field
+            </div>
+
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Accept our terms and conditions</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign up</button>
+
+            <div>
+                Already registered?
+                <a href="login.php" class="btn btn-outline-dark">Login</a>
+
+            </div>
+
+        </form>
+    </div>
+
+    <script>
+        function verifyPasswords() {
+            var p1 = document.getElementById("pass1").value;
+            var p2 = document.getElementById("pass2").value;
+            if (p1 != p2) {
+                document.getElementById("alert").style.visibility = "visible";
+                return false;
+            } else {
+                return true;
+            }
+
+        }
+
+        function hideError() {
+            document.getElementById("alert").style.visibility = "hidden";
+        }
+    </script>
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <footer>
+        <p>All rights reserved 2021®. Designed by Thesara Subasinghe</p>
+
+    </footer>
+</body>
+
+</html>
